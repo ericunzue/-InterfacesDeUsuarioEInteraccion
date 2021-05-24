0e8fb72df4babd37e1f641540322c076be14f553
ericunzue <ericunzue@gmail.com>
2021-05-23 22:24:35 -0300

    add preload

--- entregable-3-2021/index.js
+++ entregable-3-2021/index.js
@@ -1,4 +1,11 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(showPage, 31000);
 
-})
+});
+
+
+function showPage() {
+    document.getElementById("heart-rate").style.display = "none";
+    // document.getElementById("main").style.display = "block";
+}
