43e8e76f8ad1fabea970289ccb48f46e018669f6
ericunzue <ericunzue@gmail.com>
2021-05-31 23:43:41 -0300

    and message site under construnction

--- entregable-3-2021/index.js
+++ entregable-3-2021/index.js
@@ -1,32 +1,42 @@
 'use strict'
 document.addEventListener('DOMContentLoaded', () => {
-    alert("Site under construction. For a complete experience we recommend using chrome as your browser. Thanks!!");
-
-
+    // alert("Site under construction. For a complete experience we recommend using chrome as your browser. Thanks!!");
     let countDownDate = new Date("Aug 14, 2021 22:00:00").getTime();
-    setTimeout(showPage, 3000);
-    setTimeout(slideInDown, 3000);
-
-    // FUENTE: https://www.w3schools.com/howto/howto_js_countdown.asp
-    let x = setInterval(function() {
-        let now = new Date().getTime();
-        let remains = countDownDate - now;
-        var days = Math.floor(remains / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((remains % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((remains % (1000 * 60 * 60)) / (1000 * 60));
-
-        document.getElementById("count").innerHTML = "Release in: " + days + " days " + hours + "h " +
-            minutes + "m ";
-
-        if (remains < 0) {
-            clearInterval();
-            document.getElementById("count").innerHTML = "RELEASED"
-
-        }
+    document.getElementById("loader").style.display = "none";
 
 
+    let btnAcept = document.getElementById('acept');
+    btnAcept.addEventListener('click', () => {
+        document.getElementById("notice").style.display = "none";
+        document.getElementById("loader").style.display = "flex";
 
-    }, 1000);
+        setTimeout(showPage, 3000);
+        setTimeout(slideInDown, 3000);
+
+        // FUENTE: https://www.w3schools.com/howto/howto_js_countdown.asp
+        let x = setInterval(function() {
+            let now = new Date().getTime();
+            let remains = countDownDate - now;
+            var days = Math.floor(remains / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((remains % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((remains % (1000 * 60 * 60)) / (1000 * 60));
+
+            document.getElementById("count").innerHTML = "Release in: " + days + " days " + hours + "h " +
+                minutes + "m ";
+
+            if (remains < 0) {
+                clearInterval();
+                document.getElementById("count").innerHTML = "RELEASED"
+
+            }
+
+
+
+        }, 1000);
+
+
+    });
+
 
 });
 
